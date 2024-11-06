05d5c0a574a2d76907387d738dd8da2dd94e15d3
faishalam <faishalabdulmajid@gmail.com>
2024-11-07 04:43:19 +0700

    cek

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 release: bin/rails db:migrate
 web bin/rails server -p ${PORT:-5000} -e $RAILS_ENV
+web: bundle exec puma -C config/puma.rb
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,7 +41,8 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
 
   config.action_cable.url = "wss://chat-web-app-1358486b4ea0.herokuapp.com/cable"
-config.action_cable.allowed_request_origins = ['https://chat-web-app-1358486b4ea0.herokuapp.com', /http:\/\/chat-web-app-1358486b4ea0.herokuapp.*/]
+  config.action_cable.allowed_request_origins = ['https://chat-web-app-1358486b4ea0.herokuapp.com']
+
 
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
