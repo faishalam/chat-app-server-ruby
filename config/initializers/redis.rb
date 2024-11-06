970281fb17117b98c8084e6c520de6937a4f600b
faishalam <faishalabdulmajid@gmail.com>
2024-11-06 15:44:45 +0700

    fix

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,6 @@
-Redis.current = Redis.new(
-  url: ENV["REDIS_URL"],
-  ssl: true
-)
+# config/initializers/redis.rb
+Redis.current = Redis.new(host: 'localhost', port: 6379)  # Adjust to your configuration
+
+if defined?(Redis)
+    Redis.current = Redis.new(host: 'localhost', port: 6379)  # or use your actual Redis config
+  end
