de64c99f21b122cc87cfa314451b5e16ed44674e
faishalam <faishalabdulmajid@gmail.com>
2024-11-06 15:54:06 +0700

    done

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,8 +1,6 @@
-# config/initializers/redis.rb
 require 'redis'
 
-redis_config = { host: 'localhost', port: 6379 }
-$redis = Redis.new(redis_config)
-
-# Or if you want a pool (useful for multi-threaded environments):
-$redis_pool = ConnectionPool.new(size: 5, timeout: 5) { Redis.new(redis_config) }
+$redis = Redis.new(
+  url: "rediss://:p2efb532c2cae389bee852e2543087bd5544e086c6a24fdbb9300389d51f90316@ec2-98-84-68-178.compute-1.amazonaws.com:31390gi", 
+  ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+)
